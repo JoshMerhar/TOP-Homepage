e13e6b79f704bdeeab7f764f4646327843b79a1b
Josh Merhar <122338744+JoshMerhar@users.noreply.github.com>
2024-02-04 23:49:03 -0500

    Refine styling at breakpoints and add main images

--- dist/main.js
+++ dist/main.js
@@ -16,7 +16,7 @@
   \**************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `* {\n    box-sizing: border-box;\n}\n\nbody {\n    margin: auto;\n    background-color: cornsilk;\n}\n\n.about-section {\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    padding: 5vh 10vw;\n    background: linear-gradient(170deg, aqua 50%, cornsilk 50%);\n}\n\n.about-name {\n    color: whitesmoke;\n    font-size: 50px;\n    margin: 0;\n}\n\n.about-image {\n    z-index: 1;\n    flex-shrink: 0;\n    position: relative;\n    display: flex;\n    justify-content: center;\n    align-items: flex-start;\n    height: 450px;\n    width: 400px;\n    border: 3px solid lime;\n    background-color: slateblue;\n}\n\n.about-text-box {\n    height: fit-content;\n    background-color: whitesmoke;\n    padding: 10px;\n}\n\n.about-title {\n    font-size: 36px;\n    font-weight: bold;\n    margin: 0;\n}\n\n.about-text {\n    font-size: 24px;\n}\n\n.projects-section {\n    display: flex;\n    flex-wrap: wrap;\n    justify-content: space-between;\n    align-items: center;\n    padding: 0 10vw 5vh 10vw;\n    gap: 40px;\n}\n\n.project-card {\n    width: 425px;\n    height: 475px;\n    display: flex;\n    flex-direction: column;\n    border: 3px solid black;\n}\n\n.project-image {\n    height: 65%;\n    border: 2px solid maroon;\n}\n\n.card-description {\n    height: 35%;\n    border: 2px solid gray;\n}\n\n.contact-section {\n    height: 400px;\n    display: flex;\n    justify-content: center;\n    padding: 0 10vw;\n    background-color: aqua;\n}\n\n.contact-info,\n.contact-picture-space {\n    width: 100%;\n}\n\n@media (min-width: 769px) and (max-width: 1150px) {\n    .about-image {\n        height: 350px;\n        width: 300px;\n    }\n\n    .about-name {\n        font-size: 44px;\n    }\n    \n    .about-text {\n        font-size: 20px;\n    }\n    \n    .projects-section {\n        gap: 20px;\n    }\n    \n    .project-card {\n        width: 300px;\n        height: 365px;\n    }\n}\n\n@media (max-width: 768px) {\n    .about-section {\n        padding: 5vh 5vw;\n    }\n    \n    .about-image {\n        height: 275px;\n        width: 225px;\n    }\n\n    .about-name {\n        font-size: 36px;\n    }\n\n    .about-text-box {\n        height: fit-content;\n    }\n\n    .about-title {\n        font-size: 30px;\n    }\n\n    .about-text {\n        font-size: 16px;\n    }\n\n    .projects-section {\n        padding: 0 5vw;\n    }\n\n    .project-card {\n        width: 275px;\n        height: 325px;\n    }\n\n    .contact-section {\n        padding: 0 5vw;\n    }\n}\n\n@media (max-width: 575px) {\n    .about-section {\n        flex-direction: column;\n        align-items: center;\n    }\n\n    .about-image {\n        height: 350px;\n        width: 300px;\n        margin: auto;\n    }\n\n    .about-text-box {\n        width: 300px;\n        padding: 5px;\n        height: fit-content;\n        translate: 0;\n    }\n\n    .about-title {\n        font-size: 24px;\n    }\n\n    .about-text {\n        font-size: 14px;\n    }\n\n    .projects-section {\n        justify-content: center;\n    }\n\n    .contact-section {\n        flex-direction: column;\n        height: auto;\n        align-items: center;\n    }\n}`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://top-homepage/./src/styles.css?./node_modules/css-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/getUrl.js */ \"./node_modules/css-loader/dist/runtime/getUrl.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);\n// Imports\n\n\n\nvar ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ./images/Smaller_me_pic.JPG */ \"./src/images/Smaller_me_pic.JPG\"), __webpack_require__.b);\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\nvar ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `* {\n    box-sizing: border-box;\n}\n\nbody {\n    margin: auto;\n    background-color: gainsboro;\n}\n\nimg {\n    display: block;\n}\n\n.about-section {\n    background: linear-gradient(175deg, rgb(45, 180, 190) 50%, gainsboro 50%);\n}\n\n.about-content {\n    max-width: 1440px;\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    padding: 5vh 10vw;\n    margin: auto;\n}\n\n.about-name {\n    color: black;\n    font-size: 50px;\n    margin: 0;\n}\n\n.about-image {\n    z-index: 1;\n    flex-shrink: 0;\n    position: relative;\n    display: flex;\n    justify-content: center;\n    align-items: flex-start;\n    height: 450px;\n    width: 400px;\n    background-image: url(${___CSS_LOADER_URL_REPLACEMENT_0___});\n    background-size: cover;\n    border-radius: 15px;\n}\n\n.about-text-box {\n    height: fit-content;\n    margin-left: -25px;\n    background-color: whitesmoke;\n    padding: 2rem;\n    border-radius: 15px;\n}\n\n.about-title {\n    font-size: 36px;\n    font-weight: bold;\n    margin: 0;\n}\n\n.about-text {\n    font-size: 24px;\n}\n\n.projects-section {\n    max-width: 1440px;\n    display: grid;\n    grid-template-columns: repeat(auto-fit, minmax(275px, 1fr));\n    justify-items: center;\n    align-items: center;\n    padding: 0 10vw 5vh 10vw;\n    grid-gap: 2rem;\n    margin: auto;\n}\n\n.project-card {\n    width: 100%;\n    height: 475px;\n    display: flex;\n    flex-direction: column;\n    border-radius: 15px;\n    box-shadow: 7px 7px 3px darkslategray;\n}\n\n.project-image {\n    height: 65%;\n    object-fit: contain;\n    background-color: rgb(45, 180, 190);\n    border-radius: 15px 15px 0 0;\n}\n\n.card-description {\n    height: 35%;\n    background-color: whitesmoke;\n    padding: 1rem;\n    border-radius: 0 0 15px 15px;\n}\n\n.project-title-line {\n    display: flex;\n    justify-content: space-between;\n}\n\n.project-title {\n    margin: 0 0 20px 0;\n}\n\n.project-description {\n    font-size: 18px;\n}\n\n.contact-section {\n    background-color: rgb(45, 180, 190);\n}\n\n.contact-content {\n    max-width: 1440px;\n    height: 400px;\n    display: flex;\n    justify-content: space-between;\n    align-items: center;\n    padding: 0 10vw;\n    margin: auto;\n}\n\n.contact-info {\n    height: 100%;\n}\n\n.contact-picture {\n    display: block;\n    height: 95%;\n    margin: 1rem 0;\n    object-fit: cover;\n    border-radius: 15px;\n}\n\n@media (min-width: 769px) and (max-width: 1150px) {\n    .about-image {\n        height: 350px;\n        width: 300px;\n    }\n\n    .about-name {\n        font-size: 44px;\n    }\n    \n    .about-text {\n        font-size: 20px;\n    }\n    \n    .project-card {\n        width: 300px;\n        height: 425px;\n    }\n\n    .project-description {\n        font-size: 16px;\n    }\n}\n\n@media (max-width: 768px) {\n    .about-content {\n        padding: 5vh 5vw;\n    }\n    \n    .about-image {\n        height: 275px;\n        width: 225px;\n    }\n\n    .about-name {\n        font-size: 36px;\n    }\n\n    .about-text-box {\n        height: fit-content;\n    }\n\n    .about-title {\n        font-size: 30px;\n    }\n\n    .about-text {\n        font-size: 16px;\n    }\n\n    .projects-section {\n        padding: 0 5vw 3vh 5vw;\n    }\n\n    .contact-content {\n        padding: 0 5vw;\n    }\n}\n\n@media (max-width: 575px) {\n    .about-content {\n        flex-direction: column;\n        align-items: center;\n    }\n\n    .about-image {\n        height: 350px;\n        width: 300px;\n        margin: auto;\n        border-radius: 15px 15px 0 0;\n    }\n\n    .about-text-box {\n        width: 300px;\n        padding: .5rem;\n        height: fit-content;\n        margin-left: 0;\n        border-radius: 0 0 15px 15px;\n    }\n\n    .about-title {\n        font-size: 24px;\n    }\n\n    .about-text {\n        font-size: 14px;\n    }\n\n    .projects-section {\n        justify-content: center;\n    }\n\n    .project-card {\n        width: 300px;\n        height: 365px;\n    }\n\n    .project-description {\n        font-size: 14px;\n    }\n\n    .contact-content {\n        flex-direction: column;\n        align-items: center;\n        height: auto;\n    }\n\n    .contact-picture {\n        height: 400px;\n        padding: 0;\n    }\n}`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://top-homepage/./src/styles.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -27,6 +27,16 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((module) => {
 
 eval("\n\n/*\n  MIT License http://www.opensource.org/licenses/mit-license.php\n  Author Tobias Koppers @sokra\n*/\nmodule.exports = function (cssWithMappingToString) {\n  var list = [];\n\n  // return the list of modules as css string\n  list.toString = function toString() {\n    return this.map(function (item) {\n      var content = \"\";\n      var needLayer = typeof item[5] !== \"undefined\";\n      if (item[4]) {\n        content += \"@supports (\".concat(item[4], \") {\");\n      }\n      if (item[2]) {\n        content += \"@media \".concat(item[2], \" {\");\n      }\n      if (needLayer) {\n        content += \"@layer\".concat(item[5].length > 0 ? \" \".concat(item[5]) : \"\", \" {\");\n      }\n      content += cssWithMappingToString(item);\n      if (needLayer) {\n        content += \"}\";\n      }\n      if (item[2]) {\n        content += \"}\";\n      }\n      if (item[4]) {\n        content += \"}\";\n      }\n      return content;\n    }).join(\"\");\n  };\n\n  // import a list of modules into the list\n  list.i = function i(modules, media, dedupe, supports, layer) {\n    if (typeof modules === \"string\") {\n      modules = [[null, modules, undefined]];\n    }\n    var alreadyImportedModules = {};\n    if (dedupe) {\n      for (var k = 0; k < this.length; k++) {\n        var id = this[k][0];\n        if (id != null) {\n          alreadyImportedModules[id] = true;\n        }\n      }\n    }\n    for (var _k = 0; _k < modules.length; _k++) {\n      var item = [].concat(modules[_k]);\n      if (dedupe && alreadyImportedModules[item[0]]) {\n        continue;\n      }\n      if (typeof layer !== \"undefined\") {\n        if (typeof item[5] === \"undefined\") {\n          item[5] = layer;\n        } else {\n          item[1] = \"@layer\".concat(item[5].length > 0 ? \" \".concat(item[5]) : \"\", \" {\").concat(item[1], \"}\");\n          item[5] = layer;\n        }\n      }\n      if (media) {\n        if (!item[2]) {\n          item[2] = media;\n        } else {\n          item[1] = \"@media \".concat(item[2], \" {\").concat(item[1], \"}\");\n          item[2] = media;\n        }\n      }\n      if (supports) {\n        if (!item[4]) {\n          item[4] = \"\".concat(supports);\n        } else {\n          item[1] = \"@supports (\".concat(item[4], \") {\").concat(item[1], \"}\");\n          item[4] = supports;\n        }\n      }\n      list.push(item);\n    }\n  };\n  return list;\n};\n\n//# sourceURL=webpack://top-homepage/./node_modules/css-loader/dist/runtime/api.js?");
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
+/*!********************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/getUrl.js ***!
+  \********************************************************/
+/***/ ((module) => {
+
+eval("\n\nmodule.exports = function (url, options) {\n  if (!options) {\n    options = {};\n  }\n  if (!url) {\n    return url;\n  }\n  url = String(url.__esModule ? url.default : url);\n\n  // If url is already wrapped in quotes, remove them\n  if (/^['\"].*['\"]$/.test(url)) {\n    url = url.slice(1, -1);\n  }\n  if (options.hash) {\n    url += options.hash;\n  }\n\n  // Should url be wrapped?\n  // See https://drafts.csswg.org/css-values-3/#urls\n  if (/[\"'() \\t\\n]|(%20)/.test(url) || options.needQuotes) {\n    return \"\\\"\".concat(url.replace(/\"/g, '\\\\\"').replace(/\\n/g, \"\\\\n\"), \"\\\"\");\n  }\n  return url;\n};\n\n//# sourceURL=webpack://top-homepage/./node_modules/css-loader/dist/runtime/getUrl.js?");
 
 /***/ }),
 
@@ -118,6 +128,16 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles.css */ \"./src/styles.css\");\n\n\n//# sourceURL=webpack://top-homepage/./src/index.js?");
 
+/***/ }),
+
+/***/ "./src/images/Smaller_me_pic.JPG":
+/*!***************************************!*\
+  !*** ./src/images/Smaller_me_pic.JPG ***!
+  \***************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"523cf7e2ac598b3167ef.JPG\";\n\n//# sourceURL=webpack://top-homepage/./src/images/Smaller_me_pic.JPG?");
+
 /***/ })
 
 /******/ 	});
@@ -146,6 +166,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
@@ -171,6 +194,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -185,6 +220,55 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"main": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		// no jsonp function
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
